c0bd08e2d4445dbef709ff2af2db2822b0e305c4
Foysal Mahmud <foysal.bu.cse28@gmail.com>
2020-10-28 17:23:17 +0600

    Component Tree

---
+++ src/components/MainComponent.js
@@ -0,0 +1,17 @@
+import Body from "./body/Body";
+import Footer from "./footer/Footer";
+import Header from "./header/Header";
+
+
+const MainComponent = () => {
+    return (
+        <div>
+            <Header />
+            <Body />
+            <Footer />
+
+        </div>
+    );
+}
+
+export default MainComponent;
